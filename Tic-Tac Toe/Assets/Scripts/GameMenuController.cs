b7e1a9f1788359943bf1c5bc9a09b1ffd2d3ada6
Rodolfo Gomez <rgomezer@gmail.com>
2018-10-27 14:28:23 -0500

    Implemented menu is game scene

---
+++ Tic-Tac Toe/Assets/Scripts/GameMenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuController : MonoBehaviour {
+
+    //Main Game Menu
+    [SerializeField] private GameObject backButtonObj;
+    [SerializeField] private GameObject quitButtonObj;
+
+    // Use this for initialization
+    void Start () {
+
+        //safety
+        Debug.Assert(quitButtonObj != null);
+        Debug.Assert(backButtonObj != null);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GoBackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
