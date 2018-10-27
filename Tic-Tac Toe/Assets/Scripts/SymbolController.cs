23ad578ef797716cd23a07a030fada9b11c188c0
Rodolfo Gomez <rgomezer@gmail.com>
2018-10-27 03:47:11 -0500

    Refactoring and implemented SymbolController

---
+++ Tic-Tac Toe/Assets/Scripts/SymbolController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SymbolController : MonoBehaviour {
+
+    private string playerSymbol;
+    private string opposingSymbol;
+
+	// Use this for initialization
+	void Start () {
+
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetPlayerSymbol(string other)
+    {
+        this.playerSymbol = other;
+    }
+
+    public void SetOpposingSymbol(string other)
+    {
+        this.opposingSymbol = other;
+    }
+
+    public string getPlayerSymbol
+    {
+        get
+        {
+            return this.playerSymbol;
+        }
+    }
+
+    public string getOpposingSymbol
+    {
+        get
+        {
+            return this.opposingSymbol;
+        }
+    }
+}
