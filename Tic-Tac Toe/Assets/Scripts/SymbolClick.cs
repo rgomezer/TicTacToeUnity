b4b710f367370c74c5bd8398bfb2c28daccf968e
Rodolfo Gomez <rgomezer@gmail.com>
2018-10-27 16:43:11 -0500

    Programmed interactivity with the buttons

---
+++ Tic-Tac Toe/Assets/Scripts/SymbolClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SymbolClick : MonoBehaviour {
+
+    //reference to symbol controller script
+    private SymbolController pSymbolController;
+
+    // Use this for initialization
+    void Start () {
+
+        pSymbolController = GameObject.Find("SymbolController").GetComponent<SymbolController>();
+        Debug.Assert(pSymbolController != null); //safety
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClickSymbolButton()
+    {
+        gameObject.GetComponentInChildren<Text>().text = pSymbolController.getPlayerSymbol;
+        gameObject.GetComponent<Button>().interactable = false;
+    }
+}
